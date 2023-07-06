0812287b7778698391333f2689c85f26b85f4c14
AZURIIEAL <abinbinu678@gmail.com>
2023-07-06 12:05:12 +0530

    c#A3

--- C#_ASP.NetCore/Assignment3/Assignment3/Program.cs
+++ C#_ASP.NetCore/Assignment3/Assignment3/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+                                 ASSIGNMENT NO 3 (06/07/2023)
+________________________________________________________________________________
+Write a program to display the below details of a given input string:
+Length of the string
+Index of a particular character
+Reverse of the string
+Append Hai at the beginning of the given string
+Last character of the string
+Replace ‘a’ by ‘_’
+Calculate occurrence of given character in a string.
+*/
+
+//Rewrite the above program using stringbuilder
+
+/*
+ * Write a program to do the below datetime calculation
+Print the current date and time in different formats
+Find the difference between two dates
+Display Day, Month, Year of a given Date
+To a given Date, add 10 days
+
+ */
+
+//Single Calculate  method, Calculate out add, out sub, out mult, out div
+//_________________________________________________________________________________
+
+
+
+
+
+
+
