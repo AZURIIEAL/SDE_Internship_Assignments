97f191391549a5aed410d47790c344168d13b0f7
AZURIIEAL <abinbinu678@gmail.com>
2023-07-13 19:14:14 +0530

    SQL-13/7

--- SQL-SSMS/Assignment_08_13th_July/10_Queries_2019.sql
+++ SQL-SSMS/Assignment_08_13th_July/10_Queries_2019.sql
@@ -1,4 +1,6 @@
 
+--    ASSIGNMENT:08
+--<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 --1. What is the Name, ProductID and Weight for the item with Product Number 'RM-M823'
 SELECT Name,ProductID,Weight from SalesLT.Product WHERE ProductNumber = 'RM-M823'
 
@@ -14,7 +16,7 @@ SELECT COUNT(*) FROM SalesLT.Product WHERE 400 < [Weight] AND [Weight] <500;
 SELECT COUNT(*) FROM SalesLT.Product WHERE [Weight] BETWEEN 400 AND 500;
 
 --4. How many Yellow colored bikes are there (Bikes can be identified by the Product number starting with 'BK')
-SELECT COUNT(*) FROM SalesLT.Product WHERE ProductNumber LIKE 'BK%' AND Color='Yellow'
+SELECT COUNT(*) AS Bikes FROM SalesLT.Product WHERE ProductNumber LIKE 'BK%' AND Color='Yellow' 
 
 --5. Find average, maximum and minimum list price for each Product line (exclude products where Product line is NULL), sort descending by Average Standard Cost.
 SELECT ExtractString,AVG(ListPrice) AvgListPrice,MAX(ListPrice) MaxListPrice,MIN(ListPrice) MinListPrice,AVG(StandardCost) AvgStdCost
@@ -25,11 +27,31 @@ FROM(
 GROUP BY ExtractString 
 ORDER BY (AvgStdCost) DESC;
 
+select * from SalesLT.Product
 
+--__________________________Evening___________13 th JULY 2023________________________________________________________________________________________
 
+--1.Use the SubTotal value in SaleOrderHeader to list orders from the largest to the smallest. For each order show the CompanyName and the SubTotal and the total weight of the order.
+SELECT A.CompanyName, B.SubTotal, SUM( D.Weight * C.OrderQty)
+FROM SalesLT.Customer A
+INNER JOIN SalesLT.SalesOrderHeader B
+	ON A.CustomerID = B.CustomerID
+INNER JOIN SalesLT.SalesOrderDetail C
+	ON B.SalesOrderID = C.SalesOrderID
+INNER JOIN SalesLT.Product D
+	ON C.ProductID = D.ProductID
+GROUP BY A.CompanyName, B.SubTotal
+ORDER BY B.SubTotal DESC
 
+--2. How many products in ProductCategory ‘Cranksets’ have been sold to an address in ‘London’
+SELECT * From SalesLT.ProductCategory WHERE Name='Cranksets'
 
-
-
-
+SELECT *
+FROM ( 
+SELECT * FROM SalesLT.ProductCategory pc
+INNER JOIN SalesLT.Product sp ON pc.ProductCategoryID = sp.ProductCategoryID
+INNER JOIN SalesLT.SalesOrderDetail sod ON sp.ProductID = sod.ProductID
+INNER JOIN SalesLT.SalesOrderHeader soh ON pc.ProductCategoryID = sp.ProductCategoryID
+INNER JOIN SalesLT.Product sp ON pc.ProductCategoryID = sp.ProductCategoryID
+)t
 
