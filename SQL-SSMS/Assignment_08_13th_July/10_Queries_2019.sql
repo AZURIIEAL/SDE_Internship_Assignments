f679c09920559932b9dd29daee55706a3faa41af
AZURIIEAL <abinbinu678@gmail.com>
2023-07-14 18:36:31 +0530

    SQL-13/7

--- SQL-SSMS/Assignment_08_13th_July/10_Queries_2019.sql
+++ SQL-SSMS/Assignment_08_13th_July/10_Queries_2019.sql
@@ -44,7 +44,7 @@ GROUP BY A.CompanyName, B.SubTotal
 ORDER BY B.SubTotal DESC
 
 --2. How many products in ProductCategory ‘Cranksets’ have been sold to an address in ‘London’
-SELECT * FROM SalesLT.ProductCategory pc
+SELECT COUNT(*) FROM SalesLT.ProductCategory pc
 INNER JOIN SalesLT.Product sp 
 	ON pc.ProductCategoryID = sp.ProductCategoryID
 INNER JOIN SalesLT.SalesOrderDetail sod 
@@ -58,6 +58,71 @@ INNER JOIN SalesLT.CustomerAddress CA
 INNER JOIN SalesLT.[Address] A 
 	ON CA.AddressID = A.AddressID
 WHERE pc.Name='Cranksets' AND City='London'
+
+/*3.For every customer with a ‘Main Office’ in Dallas show AddressLine1 of the ‘Main Office’ and AddressLine1 of the ‘Shipping’ address
+- if there is no shipping address leave it blank. Use one row per customer.*/
+
+
+
+SELECT 
+    MO.AddressLine1 AS MainOfficeAddressLine1,
+    ISNULL(SA.AddressLine1, '') AS ShippingAddressLine1
+FROM 
+    SalesLT.Customer AS C
+    INNER JOIN SalesLT.CustomerAddress AS CA_MO 
+	ON C.CustomerID = CA_MO.CustomerID
+    INNER JOIN SalesLT.Address AS MO
+	ON CA_MO.AddressID = MO.AddressID
+
+    LEFT JOIN SalesLT.CustomerAddress AS CA_SA 
+	ON C.CustomerID = CA_SA.CustomerID AND CA_SA.AddressType = 'Shipping'
+    LEFT JOIN SalesLT.Address AS SA 
+	ON CA_SA.AddressID = SA.AddressID
+WHERE 
+    MO.City = 'Dallas' AND CA_MO.AddressType = 'Main Office';
+
+
+	--.....................................................................
+
+
+
+SELECT * FROM 
+    SalesLT.Customer AS C
+    INNER JOIN SalesLT.CustomerAddress AS CA_MO ON C.CustomerID = CA_MO.CustomerID
+    INNER JOIN SalesLT.Address AS MO ON CA_MO.AddressID = MO.AddressID
+	WHERE 
+    MO.City = 'Dallas'
+    LEFT JOIN SalesLT.CustomerAddress AS CA_SA ON C.CustomerID = CA_SA.CustomerID AND CA_SA.AddressType = 'Shipping'
+
+
+/*
+4.For each order show the SalesOrderID and SubTotal calculated  in three ways: 
+   A) From the SalesOrderHeader  (Order header table)
+   B) Sum of OrderQtyUnitPrice  ( Order Detail table ) 
+   C) Sum of OrderQtyListPrice (Product table)
+*/
+
+
+SELECT * FROM SalesLT.SalesOrderHeader
+SELECT * FROM SalesLT.SalesOrderDetail
+SELECT * FROM SalesLT.Product
+
+
+
+SELECT a.SalesOrderID,SubTotal,t2.SubTotal2,t1.SubTotal_3 FROM SalesLT.SalesOrderHeader as a
+INNER JOIN (SELECT SalesOrderID,SUM(UnitPrice*OrderQty) SubTotal_2 FROM SalesLT.SalesOrderDetail
+GROUP BY SalesOrderID)as t
+on a.SalesOrderID = t.SalesOrderID --
+INNER JOIN (SELECT SalesOrderID,SUM(ListPrice*OrderQty) SubTotal_3 FROM SalesLT.Product sp
+INNER JOIN SalesLT.SalesOrderDetail sod ON sod.ProductID = sp.ProductID
+GROUP BY SalesOrderID) as t1 
+on t.SalesOrderID = t1.SalesOrderID --
+INNER JOIN (SELECT SalesOrderID,SUM(UnitPrice*OrderQty) SubTotal2 FROM SalesLT.SalesOrderDetail
+GROUP BY SalesOrderID
+)as t2 on t.SalesOrderID = t2.SalesOrderID
+
+
+
 
 
 
