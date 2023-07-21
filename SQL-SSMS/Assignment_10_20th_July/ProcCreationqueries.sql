abf338b3e6a84a58e2625351795e2851ea5dfdd9
AZURIIEAL <abinbinu678@gmail.com>
2023-07-21 17:49:47 +0530

    SQL 21/07/23 3

--- SQL-SSMS/Assignment_10_20th_July/ProcCreationqueries.sql
+++ SQL-SSMS/Assignment_10_20th_July/ProcCreationqueries.sql
@@ -34,42 +34,66 @@ END;
 GO
 
 --Create the stored procedure to insert data into the Location table
-CREATE PROCEDURE InsertLocation 
-    @Location VARCHAR(50)
+CREATE PROCEDURE AddLocation 
+    @Zipcode INT,
+	@Name VARCHAR(50)
 AS
 BEGIN
-    INSERT INTO MovieMate.[Location]([Location])
-    VALUES (@Location);
+    INSERT INTO MovieMate.[Location]([Zipcode],[Name])
+    VALUES (@Zipcode,@Name);
 END;
 
+GO
 
+--Create the stored procedure to insert data into the Director table
+CREATE PROCEDURE AddDirector
+	@DirectorName VARCHAR(255)
+AS
+BEGIN
+    INSERT INTO MovieMate.[Director]([DirectorName])
+    VALUES (@DirectorName);
+END;
 
+GO
 
+--Create the stored procedure to insert data into the Screen table
+CREATE PROCEDURE AddScreen
+	@Name VARCHAR(255),
+	@Capacity INT
 
+AS
+BEGIN
+    INSERT INTO MovieMate.[Screen]([Name],Capacity)
+    VALUES (@Name ,@Capacity);
+END;
 
+GO
 
+--Create the stored procedure to insert data into the SeatCategory table
+CREATE PROCEDURE AddSeatCategory
+	@TypeName VARCHAR(50),
+	@Price DECIMAL(10,2)
+AS
+BEGIN
+    INSERT INTO MovieMate.[SeatCategory](TypeName,Price)
+    VALUES (@TypeName ,@Price);
+END;
 
+GO
 
+--Create the stored procedure to insert data into the Time table
+CREATE PROCEDURE AddTime
+	@ShowTimeTitle VARCHAR(50)
+AS
+BEGIN
+    INSERT INTO MovieMate.[Time](ShowTimeTitle)
+    VALUES (@ShowTimeTitle);
+END;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+GO
 
 -- Create the stored procedure to insert data into the User table
-CREATE PROCEDURE InsertUser 
+CREATE PROCEDURE AddUser 
     @Username VARCHAR(50),
     @Email VARCHAR(100),
     @Password VARCHAR(100),
@@ -80,21 +104,51 @@ BEGIN
     VALUES (@Username, @Email, @Password, @Address);
 END;
 
+GO
 
-
+--Creating a type
+CREATE TYPE MovieMate.EntityId AS TABLE(
+    [Id] int 
+);
+GO
 -- Create the stored procedure to insert data into the Movie table
-CREATE PROCEDURE InsertMovie
+CREATE PROCEDURE AddMovie
     @Title VARCHAR(100),
     @Certification INT,
     @ReleaseDate DATE,
     @Duration INT,
-    @PosterLink VARCHAR(200),
+    @PosterLink VARCHAR(200), 
     @TrailerLink VARCHAR(200),
-	@Description TEXT
+	@Description TEXT,
+    @GenreIds AS MovieMate.EntityId READONLY
 AS
 BEGIN
     INSERT INTO MovieMate.Movie (Title, Certification, ReleaseDate, Duration, PosterLink, TrailerLink,[Description])
     VALUES (@Title, @Certification, @ReleaseDate, @Duration, @PosterLink, @TrailerLink,@Description);
+
+-- @@Identity @scope_identity
+	DECLARE @MovieId INT = @@Identity
+
+	INSERT MovieMate.MovieGenre (MovieId, GenreId)
+	SELECT @MovieId, Id
+	FROM @GenreIds
 END;
 
+GO
 
+
+DECLARE @GenreIds AS MovieMate.EntityId
+INSERT @GenreIds
+SELECT 1 UNION ALL
+SELECT 2 UNION ALL
+SELECT 3
+
+EXEC AddMovie
+	@Title = 'Titanic',
+    @Certification = 'u',
+    @ReleaseDate = '2021-01-01',
+    @Duration = 1,
+    @PosterLink = NULL, 
+    @TrailerLink = NULL,
+	@Description = NULL,
+	@GenreIds = @GenreIds
