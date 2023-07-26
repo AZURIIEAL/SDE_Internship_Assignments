b03f53c55f3f79786d10011866b8948367d24915
AZURIIEAL <abinbinu678@gmail.com>
2023-07-26 13:04:16 +0530

    Added the js assignment

---
+++ JS _Angular_Assignments/Assignment_01_26thJuly/script.js
@@ -0,0 +1,10 @@
+
+// Get the button element by its id
+    var alertButton = document.getElementById("alertButton");
+
+    // Add a click event listener to the button
+    alertButton.addEventListener("click", function () {
+        //Using an anonymous function and triggering it by id
+    // Show an alert box with the message "Hello World" when the button is clicked
+    alert("Hello World");
+    });
--- SQL-SSMS/Assignment_11_24th_July/Queries.sql
+++ SQL-SSMS/Assignment_11_24th_July/Queries.sql
@@ -3,35 +3,67 @@ CREATE TYPE MovieMate.FilterItems AS TABLE(
 [Items] INT
 );
 GO
-CREATE PROCEDURE FilterMovies
+CREATE OR ALTER PROCEDURE FilterMovies
 	@Location INT,
 	@Genre AS MovieMate.FilterItems READONLY ,
 	@Language AS MovieMate.FilterItems READONLY
 
 	AS 
 		BEGIN
-			SELECT * 
-			FROM 
-			 (
-				SELECT L.* 
+			SELECT DISTINCT ST.* 
 				FROM MovieMate.[Location] L
-				LEFT JOIN MovieMate.Theatre T ON L.LocationId = T.LocationId AND L.LocationId = @Location
+				LEFT JOIN MovieMate.Theatre T ON L.LocationId = T.LocationId 
 				LEFT JOIN MovieMate.Screen S ON T.TheatreId =S.TheatreId
-				LEFT JOIN MovieMate.ShowTime ST ON S.ScreenId = ST.ScreenId
+				LEFT JOIN MovieMate.ShowTime ST ON S.ScreenId = ST.ScreenId AND ST.Date > CAST(GETDATE() AS DATE)
 				LEFT JOIN MovieMate.MovieLanguage ML ON ST.MovieLanguageId = ML.MovieLanguageId 
 				LEFT JOIN MovieMate.MovieGenre MG ON MG.MovieId = ML.MovieId 
 				WHERE
-				(@Location IS NULL OR L.LocationId = @Location)
-				AND (NOT EXISTS(SELECT 1 FROM @Genre) OR MG.MovieGenreId IN (SELECT Items FROM @Genre))
-				AND (NOT EXISTS(SELECT 1 FROM @Language) OR ML.MovieLanguageId IN (SELECT Items FROM @Language))	
-			)temp
+				(L.LocationId = @Location)
+				AND ( MG.MovieGenreId IN (SELECT Items FROM @Genre))
+				AND ( ML.MovieLanguageId IN (SELECT Items FROM @Language))	
+
+	END;
+
+	--EXEC---
+
+	DECLARE @i MovieMate.FilterItems --For Genre
+	INSERT INTO @i ([Items]) VALUES ( 1);
+	INSERT INTO @i ([Items]) VALUES ( 3);
+	INSERT INTO @i ([Items]) VALUES (4 );
+
+
+	DECLARE @j MovieMate.FilterItems --For Language
+	INSERT INTO @j ([Items]) VALUES ( 1);
+	EXEC FilterMovies
+	@Location =2,
+	@Genre =  @i,
+	@Language =@j
+
+go
+
+
+--2. GetShowDetails by Movie Language Id
+CREATE OR ALTER PROCEDURE FilterByLanguageId
+	@LanguageId AS MovieMate.FilterItems READONLY
+	AS 
+		BEGIN 
+			SELECT * FROM MovieMate.ShowTime ST
+			LEFT JOIN MovieMate.MovieLanguage ML ON ML.MovieLanguageId = ST.MovieLanguageId
+			WHERE  ML.LanguageId IN (SELECT [Items] FROM @LanguageId);
+
 	END;
 
 
--- LocationId
--- Genre
--- 
+	--EXEC---
+	DECLARE @i MovieMate.FilterItems --For Genre
+	INSERT INTO @i ([Items]) VALUES ( 1);
 
+	EXEC FilterByLanguageId
+	@LanguageId = @i
+
+
+
+--3.GetSeatInfoByShowId
 
 
 
