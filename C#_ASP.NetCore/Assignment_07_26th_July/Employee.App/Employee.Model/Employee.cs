8481fe1b30057b52ae31efb444940974330a15e5
AZURIIEAL <abinbinu678@gmail.com>
2023-07-26 18:40:53 +0530

    c# 26/07/2023 2

---
+++ C#_ASP.NetCore/Assignment_07_26th_July/Employee.App/Employee.Model/Employee.cs
@@ -0,0 +1,30 @@
+﻿
+namespace Employee.Model
+{
+    public class Employee
+    {
+        public string Id { get; private set; }
+        public string Name { get; set; }
+        public double Salary { get; set; }
+        public string EmployeeType { get; set; }
+
+        // Static fields to count the total number of employees
+        private static int employeeCount = 0;
+
+        // Static constructor to initialize the count
+        static Employee()
+        {
+            employeeCount = 0;
+        }
+
+        // Constructor to create an employee object with auto-generated Id
+        public Employee(string name, double salary, string employeeType)
+        {
+            employeeCount++;
+            Id = "Emp" + employeeCount.ToString("D4");
+            Name = name;
+            Salary = salary;
+            EmployeeType = employeeType;
+        }
+    }
+}
