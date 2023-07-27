56ed11cef5932f214acc1260ea1b167d30e70c11
AZURIIEAL <abinbinu678@gmail.com>
2023-07-27 11:03:36 +0530

    c# 27/07/2023 2

---
+++ C#_ASP.NetCore/Assignment_07_26th_July/Employee.App/Program.cs
@@ -0,0 +1,32 @@
+﻿ 
+class program
+{
+
+    // Method to print employee details
+    static void PrintEmployeeDetails(Employee emp)
+    {
+        Console.WriteLine($"Id: {emp.Id}, Name: {emp.Name}, Salary: {emp.Salary}, Employee Type: {emp.EmployeeType}");
+    }
+
+    // Method to get the total number of employees
+
+    static void Main()
+    {
+        // Create employee objects
+        Employee emp1 = new Employee("John Doe", 15000, "Permanent");
+        Employee emp2 = new Employee("Liam Smith", 20000, "Permanent");
+        Employee emp3 = new Employee("Mary James", 15000, "Contract");
+
+        // Print the details of the employees
+        PrintEmployeeDetails(emp1);
+        PrintEmployeeDetails(emp2);
+        PrintEmployeeDetails(emp3);
+
+        // Get the total number of employees
+        int totalEmployees = Employee.GetTotalEmployeeCount();
+        Console.WriteLine("Total number of employees: " + totalEmployees);
+    }
+
+
+}
+
---
+++ C#_ASP.NetCore/Assignment_07_26th_July/Employee.model/Employee.Model.cs
@@ -0,0 +1,34 @@
+﻿public class Employee
+{
+    public string Id { get; private set; } //ReadOnly property
+    public string Name { get; set; }
+    public double Salary { get; set; }
+    public string EmployeeType { get; set; }
+
+    // Static fields to count the total number of employees
+    private static int employeeCount;
+
+    // Static constructor to initialize the count
+    static Employee()
+    {
+        //This field will be used to keep track of the total number of employees created.
+        employeeCount = 0;
+    }
+    public static int GetTotalEmployeeCount()
+    {
+        // We can access the employeeCount static field directly from the Employee class.
+        return Employee.employeeCount;
+    }
+
+    // Constructor to create an employee object with auto-generated Id
+    public Employee(string name, double salary, string employeeType)
+    {
+        employeeCount++;
+        int x = employeeCount + 1000;
+        Id = "Emp" + x.ToString();
+        Name = name;
+        Salary = salary;
+        EmployeeType = employeeType;
+    }
+}
+
