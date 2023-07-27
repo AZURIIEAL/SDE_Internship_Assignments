4c30c4e7cb8363de0b51242fb36232a9b4fe40ca
AZURIIEAL <abinbinu678@gmail.com>
2023-07-27 15:25:33 +0530

    c# 27/07/2023 5

---
+++ C#_ASP.NetCore/Assignment_07_26th_July/Assignment_5/Assignment5/Program.cs
@@ -0,0 +1,18 @@
+﻿class Program
+{
+    static void Main()
+    {
+        int[] originalArray = { 1, 2, 3, 4, 5 };//Array  to be iterated on
+
+        List<int> multipliedList = new List<int>(); //New list
+
+        foreach (int num in originalArray)
+        {
+            int multipliedNum = num * 2;
+            multipliedList.Add(multipliedNum);
+        }
+
+        Console.WriteLine("Original Array: " + string.Join(", ", originalArray));
+        Console.WriteLine("Multiplied List: " + string.Join(", ", multipliedList));
+    }
+}
