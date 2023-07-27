8a9a35c6b19f0fb0c7cc3568b9967bc35ae6dad1
AZURIIEAL <abinbinu678@gmail.com>
2023-07-27 11:39:08 +0530

    c# 27/07/2023 3

---
+++ C#_ASP.NetCore/Assignment_07_26th_July/Assignment_2/Assignment2/InvalidUserInputException.cs
@@ -0,0 +1,13 @@
+﻿
+namespace Assignment2
+{
+    // Custom exception class for invalid user input.
+    public class InvalidUserInputException : Exception //Inheriting from exception class.
+    {
+        //Now setting up the Method.
+        public InvalidUserInputException(string message) : base(message) //We are addding a base clause because we need to revert a message back.
+        {
+        }
+    }
+
+}
---
+++ C#_ASP.NetCore/Assignment_07_26th_July/Assignment_2/Assignment2/Program.cs
@@ -0,0 +1,44 @@
+﻿using Assignment2;
+
+class Program
+{
+    // Method to get sports name corresponding to the input character
+    static string GetSportsName(char input)
+    {
+        switch (input)
+        {
+            case 'c':
+                return "Cricket";
+            case 'f':
+                return "Football";
+            case 'h':
+                return "Hockey";
+            case 't':
+                return "Tennis";
+            case 'b':
+                return "Badminton";
+            default:
+                //Specifying the message which we want to convey ().
+                throw new InvalidUserInputException("Invalid input! Please enter a valid character (c, f, h, t, or b).");
+        }
+
+    }
+    //SVM driver function
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter a character (c, f, h, t, or b) to get the corresponding sports name:");
+        char userInput = Console.ReadKey().KeyChar;
+
+
+        //Try Catch block for exception handling.
+        try
+        {
+            string sportsName = GetSportsName(userInput);
+            Console.WriteLine($"\nSports Name: {sportsName}");
+        }
+        catch (InvalidUserInputException ex)
+        {
+            Console.WriteLine($"\nError: {ex.Message}");
+        }
+    }
+}
