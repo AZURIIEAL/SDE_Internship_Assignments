5444cea5df7cb41d8f6026338f983124c855c847
AZURIIEAL <abinbinu678@gmail.com>
2023-08-02 18:46:15 +0530

    .Net 02/08/2023

---
+++ C#_ASP.NetCore/DotNetCoreWeb/DependencyInjection/DependencyInjection/Models/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace DependencyInjection.Models
+{
+
+    //This app will need a temporary storage for storing some products. To keep things simple, we will be storing these products in an “In-Memory Repository” and not in the database
+    public class Product
+    {
+        public string Name { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+    }
+}
