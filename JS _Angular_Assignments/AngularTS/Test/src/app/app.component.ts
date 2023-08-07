519e705883a7fe22ee228b1bcd2a0933c5cf42a0
AZURIIEAL <abinbinu678@gmail.com>
2023-08-07 19:04:25 +0530

    7th August 2023 Angular

--- JS _Angular_Assignments/AngularTS/Test/src/app/app.component.ts
+++ JS _Angular_Assignments/AngularTS/Test/src/app/app.component.ts
@@ -2,8 +2,22 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  template: `
+    <main>
+      <header class="brand-name">
+        <img
+          class="brand-logo"
+          src="/assets/logo.svg"
+          alt="logo"
+          aria-hidden="true"
+        />
+      </header>
+      <section>
+        <app-home></app-home>
+      </section>
+    </main>
+  `,
+  styleUrls: ['./app.component.sass'],
 })
 export class AppComponent {
   title = 'Test';
