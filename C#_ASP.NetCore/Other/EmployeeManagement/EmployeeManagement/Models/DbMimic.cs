4a5ea9766c92e50660a80157478cdcb572122c52
AZURIIEAL <abinbinu678@gmail.com>
2023-08-07 13:51:59 +0530

    Angular done

---
+++ C#_ASP.NetCore/Other/EmployeeManagement/EmployeeManagement/Models/DbMimic.cs
@@ -0,0 +1,15 @@
+﻿namespace EmployeeManagement.Models
+{
+    //Should declare this class as static as we are trying to imitate a database.
+    public static class DbMimic
+    {
+        //Declaring a list to hold the entries that will be stored here in runtime.
+         private static List<Employee> allemployees = new List<Employee>();
+
+        //Now declaring a method to create entries-- the CREATE part of CRUD
+        public static void Create(Employee employee)
+        {
+            allemployees.Add(employee); 
+        }
+    }
+}
