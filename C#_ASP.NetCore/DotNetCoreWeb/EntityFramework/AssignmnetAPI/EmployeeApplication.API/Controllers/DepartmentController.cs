b3bd13e347504d714f5d162eb2653f8c72660611
AZURIIEAL <abinbinu678@gmail.com>
2023-08-16 17:53:21 +0530

    c# ASP

--- C#_ASP.NetCore/DotNetCoreWeb/EntityFramework/AssignmnetAPI/EmployeeApplication.API/Controllers/DepartmentController.cs
+++ C#_ASP.NetCore/DotNetCoreWeb/EntityFramework/AssignmnetAPI/EmployeeApplication.API/Controllers/DepartmentController.cs
@@ -1,47 +1,123 @@
 ﻿using EmployeeApplication.Model.Models;
 using EmployeeApplication.Repository.Context;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace EmployeeApplication.API.Controllers
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class DepartmentController : ControllerBase
+[Route("api/[controller]")]
+[ApiController]
+public class DepartmentController : ControllerBase
+{
+    private readonly EmployeeApplicationDbContext _dbContext;
+
+    public DepartmentController(EmployeeApplicationDbContext dbContext)
     {
-        private readonly EmployeeApplicationDbContext? dbContext;
 
-        public DepartmentController(EmployeeApplicationDbContext dbContext)
+        this._dbContext = dbContext;
+    }
+    [HttpPost("add-department")]
+    public async Task<IActionResult> AddAsync([FromQuery] DepartmentDuplicate dept)
+    {
+        try
         {
-
-            this.dbContext = dbContext;
+            Department departmentNew = new Department{
+                Name = dept.Name,
+                LocationId = dept.LocationId,
+            };
+            _dbContext.Add(departmentNew);
+            await _dbContext.SaveChangesAsync();
+            return Ok(dept);
         }
-        [HttpPost("Add")]
-        public IActionResult Add([FromQuery] Department department)
+        catch (Exception ex)
         {
-            return Ok(department);
-        }
-        [HttpDelete("Delete/{id}")]
-        public IActionResult Delete([FromQuery] int id)
-        {
-            return Ok(0);
-        }
-        [HttpGet("GetById/{id}")]
-        public string GetById(int id)
-        {
-            return "value";
-        }
-        [HttpGet("GetAll")]
-        public ActionResult<Department> GetAll()
-        {
-            return Ok();
+            return StatusCode(500, ex);
         }
 
-
-        [HttpPut("Update/{id}")]
-        public void Update([FromBody] Department department)
+    }
+    [HttpDelete("delete-by-id/{id}")]
+    public async Task<IActionResult> DeleteAsync([FromQuery] int id)
+    {
+        try
         {
+            var toDelete = await _dbContext.Departments.FindAsync(id);
+            if (toDelete is null)
+            {
+                return NotFound();
+            }
+
+            _dbContext.Departments.Remove(toDelete);
+            await _dbContext.SaveChangesAsync();
+
+            return Ok("Deleted");
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, ex);
         }
     }
+
+
+    [HttpGet("get-by-id/{id}")]
+    public async Task<IActionResult> GetDataAsync(int id)
+    {
+        try
+        {
+            var toGet = await _dbContext.Departments.FindAsync(id);
+            if (toGet == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return Ok(toGet);
+            }
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, ex);
+        }
+    }
+
+    [HttpGet("get-all")]
+    public async Task<ActionResult<Location>> GetAllAsync()
+    {
+        try
+        {
+            var locations = await _dbContext.Departments.ToListAsync();
+            return Ok(locations);
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, ex);
+        }
+    }
+
+
+    [HttpPut("update-by-id")]
+    public async Task<IActionResult> UpdatebyIdAsync([FromQuery] Department department)
+    {
+        try
+        {
+            Department? toUpdate = await _dbContext.Departments.FindAsync(department.Id);
+            if (toUpdate is null)
+            {
+                return NotFound();
+            }
+            if (string.IsNullOrWhiteSpace(department.Name))
+            {
+                return UnprocessableEntity();
+            }
+            toUpdate.Name = department.Name;
+            await _dbContext.SaveChangesAsync();
+            return Ok(toUpdate);
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, ex);
+        }
+    }
+}
 }
--- C#_ASP.NetCore/DotNetCoreWeb/EntityFramework/AssignmnetAPI/EmployeeApplication.API/Controllers/LocationController.cs
+++ C#_ASP.NetCore/DotNetCoreWeb/EntityFramework/AssignmnetAPI/EmployeeApplication.API/Controllers/LocationController.cs
@@ -6,116 +6,116 @@ using Microsoft.EntityFrameworkCore;
 
 namespace EmployeeApplication.API.Controllers
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class LocationController : ControllerBase
+[Route("api/[controller]")]
+[ApiController]
+public class LocationController : ControllerBase
+{
+    private readonly EmployeeApplicationDbContext _dbContext;
+
+    public LocationController(EmployeeApplicationDbContext dbContext)
     {
-        private readonly EmployeeApplicationDbContext _dbContext;
 
-        public LocationController(EmployeeApplicationDbContext dbContext)
-        {
+        this._dbContext = dbContext;
+    }
 
-            this._dbContext = dbContext;
-        }
-
-        [HttpPost("add-location")]
-        public async Task<IActionResult> AddAsync([FromQuery] string locationName)
-        {
-            try
-            {
-                Location locationNew = new Location { Name = locationName };
-                _dbContext.Add(locationNew);
-                await _dbContext.SaveChangesAsync();
-                return Ok(locationNew);
-            }
-            catch (Exception ex)
-            {
-                return StatusCode(500, ex);
-            }
-
-        }
-        [HttpDelete("delete-by-id/{id}")]
-        public async Task<IActionResult> DeleteAsync([FromQuery] int id)
-        {
-            try
-            {
-                var toDelete = await _dbContext.Locations.FindAsync(id);
-                if (toDelete is null)
-                {
-                    return NotFound();
-                }
-
-                _dbContext.Locations.Remove(toDelete);
-                await _dbContext.SaveChangesAsync();
-
-                return Ok("Deleted");
-            }
-            catch (Exception ex)
-            {
-                return StatusCode(500, ex);
-            }
-        }
-
-
-        [HttpGet("get-by-id/{id}")]
-        public async Task<IActionResult> GetDonationAsync(int id)
-        {
-            try
-            {
-                var toGet = await _dbContext.Locations.FindAsync(id);
-                if (toGet == null)
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    return Ok(toGet);
-                }
-            }
-            catch (Exception ex)
-            {
-                return StatusCode(500, ex);
-            }
-        }
-
-        [HttpGet("get-all")]
-        public async Task<ActionResult<Location>> GetAllAsync()
-        {
-            try
-            {
-                var locations = await _dbContext.Locations.ToListAsync();
-                return Ok(locations);
-            }
-            catch (Exception ex)
-            {
-                return StatusCode(500, ex);
-            }
-        }
-
-
-        [HttpPut("update-by-id")]
-        public async Task<IActionResult> UpdatebyIdAsync([FromQuery]  Location location)
+    [HttpPost("add-location")]
+    public async Task<IActionResult> AddAsync([FromQuery] LocationDuplicate location)
     {
         try
         {
-            Location? toUpdate = await _dbContext.Locations.FindAsync(location.Id);
-            if (toUpdate is null)
+            Location locationNew = new Location { Name = location.Name };
+            _dbContext.Add(locationNew);
+            await _dbContext.SaveChangesAsync();
+            return Ok(locationNew);
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, ex);
+        }
+
+    }
+    [HttpDelete("delete-by-id/{id}")]
+    public async Task<IActionResult> DeleteAsync([FromQuery] int id)
+    {
+        try
+        {
+            var toDelete = await _dbContext.Locations.FindAsync(id);
+            if (toDelete is null)
             {
                 return NotFound();
             }
-            if (string.IsNullOrWhiteSpace(location.Name))
-            {
-                return UnprocessableEntity();
-            }
-            toUpdate.Name = location.Name;
+
+            _dbContext.Locations.Remove(toDelete);
             await _dbContext.SaveChangesAsync();
-            return Ok(toUpdate);
+
+            return Ok("Deleted");
         }
         catch (Exception ex)
         {
             return StatusCode(500, ex);
         }
     }
+
+
+    [HttpGet("get-by-id/{id}")]
+    public async Task<IActionResult> GetDataAsync(int id)
+    {
+        try
+        {
+            var toGet = await _dbContext.Locations.FindAsync(id);
+            if (toGet == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return Ok(toGet);
+            }
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, ex);
+        }
+    }
+
+    [HttpGet("get-all")]
+    public async Task<ActionResult<Location>> GetAllAsync()
+    {
+        try
+        {
+            var locations = await _dbContext.Locations.ToListAsync();
+            return Ok(locations);
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, ex);
+        }
+    }
+
+
+    [HttpPut("update-by-id")]
+    public async Task<IActionResult> UpdatebyIdAsync([FromQuery]  Location location)
+{
+    try
+    {
+        Location? toUpdate = await _dbContext.Locations.FindAsync(location.Id);
+        if (toUpdate is null)
+        {
+            return NotFound();
+        }
+        if (string.IsNullOrWhiteSpace(location.Name))
+        {
+            return UnprocessableEntity();
+        }
+        toUpdate.Name = location.Name;
+        await _dbContext.SaveChangesAsync();
+        return Ok(toUpdate);
+    }
+    catch (Exception ex)
+    {
+        return StatusCode(500, ex);
+    }
+}
 
 }
 }
