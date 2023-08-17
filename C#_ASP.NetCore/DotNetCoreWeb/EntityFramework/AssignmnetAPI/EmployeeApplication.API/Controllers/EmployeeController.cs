b4196a5c8213b03df0125e218d32ac6326695090
AZURIIEAL <abinbinu678@gmail.com>
2023-08-17 09:22:56 +0530

    EF

--- C#_ASP.NetCore/DotNetCoreWeb/EntityFramework/AssignmnetAPI/EmployeeApplication.API/Controllers/EmployeeController.cs
+++ C#_ASP.NetCore/DotNetCoreWeb/EntityFramework/AssignmnetAPI/EmployeeApplication.API/Controllers/EmployeeController.cs
@@ -1,51 +1,127 @@
 ﻿using EmployeeApplication.Model.Models;
 using EmployeeApplication.Repository.Context;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace EmployeeApplication.API.Controllers
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class EmployeeController : ControllerBase
+[Route("api/[controller]")]
+[ApiController]
+public class EmployeeController : ControllerBase
+{
+    private readonly EmployeeApplicationDbContext _dbContext;
+
+    public EmployeeController(EmployeeApplicationDbContext dbContext)
     {
-        private readonly EmployeeApplicationDbContext? dbContext;
-
-        public EmployeeController(EmployeeApplicationDbContext dbContext)
+        this._dbContext = dbContext;
+    }
+    [HttpPost("add-employee")]
+    public async Task<IActionResult> AddAsync([FromQuery] EmployeeDuplicate emp)
+    {
+        try
         {
-            this.dbContext = dbContext;
+            Employee EmployeeNew = new Employee
+            {
+                FirstName = emp.FirstName,
+                LastName = emp.LastName,
+                Email = emp.Email,
+                PhoneNumber = emp.PhoneNumber,
+                DepartmentId = emp.DepartmentId,
+            };
+            _dbContext.Add(EmployeeNew);
+            await _dbContext.SaveChangesAsync();
+            return Ok(EmployeeNew);
         }
-        [HttpPost("Add")]
-        public IActionResult Add([FromQuery] Employee employee)
+        catch (Exception ex)
         {
-            //Need to create a new Donation object
-      
-            dbContext?.Add(employee);
-            dbContext?.SaveChanges(); 
-            return Ok(employee);
-        }
-        [HttpDelete("Delete/{id}")]
-        public IActionResult Delete([FromQuery] int id)
-        {
-            return Ok(0);
-        }
-        [HttpGet("GetById/{id}")]
-        public string GetById(int id)
-        {
-            return "value";
-        }
-        [HttpGet("GetAll")]
-        public ActionResult<Employee> GetAll()
-        {
-            return Ok();
-        }
-
-
-        [HttpPut("Update/{id}")]
-        public void Update([FromBody] Employee employee)
-        {
+            return StatusCode(500, ex);
         }
 
     }
+    [HttpDelete("delete-by-id/{id}")]
+    public async Task<IActionResult> DeleteAsync([FromQuery] int id)
+    {
+        try
+        {
+            var toDelete = await _dbContext.Departments.FindAsync(id);
+            if (toDelete is null)
+            {
+                return NotFound();
+            }
+
+            _dbContext.Departments.Remove(toDelete);
+            await _dbContext.SaveChangesAsync();
+
+            return Ok("Deleted");
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, ex);
+        }
+    }
+
+
+    [HttpGet("get-by-id/{id}")]
+    public async Task<IActionResult> GetDataAsync(int id)
+    {
+        try
+        {
+            var toGet = await _dbContext.Departments.FindAsync(id);
+            if (toGet == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return Ok(toGet);
+            }
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, ex);
+        }
+    }
+
+    [HttpGet("get-all")]
+    public async Task<ActionResult<Location>> GetAllAsync()
+    {
+        try
+        {
+            var locations = await _dbContext.Departments.ToListAsync();
+            return Ok(locations);
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, ex);
+        }
+    }
+
+
+    [HttpPut("update-by-id")]
+    public async Task<IActionResult> UpdatebyIdAsync([FromQuery] Department department)
+    {
+        try
+        {
+            Department? toUpdate = await _dbContext.Departments.FindAsync(department.Id);
+            if (toUpdate is null)
+            {
+                return NotFound();
+            }
+            if (string.IsNullOrWhiteSpace(department.Name))
+            {
+                return UnprocessableEntity();
+            }
+            toUpdate.Name = department.Name;
+            await _dbContext.SaveChangesAsync();
+            return Ok(toUpdate);
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, ex);
+        }
+    }
+
+}
 }
---
+++ C#_ASP.NetCore/DotNetCoreWeb/EntityFramework/AssignmnetAPI/EmployeeApplication.Model/Models/EmployeeDuplicate.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeApplication.Model.Models
+{
+    public class EmployeeDuplicate
+    {
+
+        [Required]
+        public string FirstName { get; set; } = string.Empty;
+
+        [Required]
+        public string LastName { get; set; } = string.Empty;
+
+        [Required]
+        public string Email { get; set; } = string.Empty;
+
+        [Required]
+        public string PhoneNumber { get; set; } = string.Empty;
+        [Required]
+        public int DepartmentId { get; set; }
+
+
+    }
+}
